35a472ce2ff780a2c6564a69f7883cb558d037c8
Shashi Ranjan <shashiranjan3840@gmail.com>
2026-06-08 22:01:05 +0530

    added Week 2 Day 1 soln

---
+++ Week1/Week2/Valid Angram.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.size() != t.size())
+            return false;
+        vector<int> count(26, 0);
+        for (int i = 0; i < s.size(); i++)
+        {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+        for (int count : count)
+        {
+            if (count != 0)
+                return false;
+        }
+        return true;
+    }
+};
