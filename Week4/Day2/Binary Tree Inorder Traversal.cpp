d2501391711c9729c370fb8354ce2071c44ffc9c
Shashi Ranjan <shashiranjan3840@gmail.com>
2026-06-23 18:58:22 +0530

    Added Week4 Day2 soln

---
+++ Week4/Day2/Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        stack<TreeNode *> st;
+        TreeNode *curr = root;
+
+        while (curr || !st.empty())
+        {
+            while (curr)
+            {
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            ans.push_back(curr->val);
+            curr = curr->right;
+        }
+
+        return ans;
+    }
+};
